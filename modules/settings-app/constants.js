d6c1f9fe51a7836e6b3cbb479baefd1330489229
yuzi83 <huzhenshi7@gmail.com>
2026-03-29 00:47:25 +0800

    Delete modules directory

--- modules/settings-app/constants.js
+++
@@ -1,9 +0,0 @@
-export const DB_PRESETS_SETTING_KEY = 'dbConfigPresets';
-export const DB_ACTIVE_PRESET_SETTING_KEY = 'activeDbConfigPreset';
-
-export const STORAGE_BUDGETS = Object.freeze({
-    backgroundImageBytes: 2 * 1024 * 1024,
-    toggleCoverBytes: 1 * 1024 * 1024,
-    appIconBytes: 512 * 1024,
-    appIconsTotalBytes: 4 * 1024 * 1024,
-});
